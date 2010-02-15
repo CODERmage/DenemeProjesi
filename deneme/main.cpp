5fe889642ea560e70f679bbab7498812a3dc49ef
CODERmage <codermage@gmail.com>
2010-02-15 22:04:33 +0200

    qt-creator commit denemesii

--- deneme/main.cpp
+++ deneme/main.cpp
@@ -1,8 +1,13 @@
 #include <QtCore/QCoreApplication>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    cout << "helo world!" << endl;
 
     return a.exec();
 }
